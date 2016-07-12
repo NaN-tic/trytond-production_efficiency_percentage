9932bb9dc1418ddadef3104348b8bb28b65d5a28
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-12 13:17:38 +0200

    Update test to version 4.0

--- tests/test_production_efficiency_percentge.py
+++ tests/test_production_efficiency_percentge.py
@@ -4,24 +4,14 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'production_efficiency_percentage')
-
-    def test0005views(self):
-        'Test views'
-        test_view('production_efficiency_percentage')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'production_efficiency_percentage'
 
 
 def suite():
@@ -30,5 +20,6 @@ def suite():
     suite.addTests(doctest.DocFileSuite(
             'scenario_production_efficiency_percentage.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
