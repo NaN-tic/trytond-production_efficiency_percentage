c209472aabde93f15626b4bd1f38793c694871ca
Albert Cervera i Areny <albert@nan-tic.com>
2018-10-26 23:33:01 +0200

    Do not use asterisk in imports.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,12 @@
 #The COPYRIGHT file at the top level of this repository contains the full
 #copyright notices and license terms.
-
 from trytond.pool import Pool
-from .production import *
+from . import production
 
 
 def register():
     Pool.register(
-        BOM,
-        BOMInput,
-        BOMOutput,
+        production.BOM,
+        production.BOMInput,
+        production.BOMOutput,
         module='production_efficiency_percentage', type_='model')
